d0aa07d61e87ac0cf025af588650c552694484ed
Cristescu Bogdan Ion <bogdan.cristescu@dcsi.eu>
2017-08-03 22:16:42 +0300

    Restarted project to adhere to package naming

---
+++ tsEarthquake/src/main/java/eu/djakarta/tsEarthquake/App.java
@@ -0,0 +1,13 @@
+package eu.djakarta.tsEarthquake;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
