675f693113336b3592a7bf098f6476427bd64549
Cristescu Bogdan Ion <bogdan.cristescu@dcsi.eu>
2017-08-03 22:19:52 +0300

    Reformat

--- src/main/java/eu/djakarta/tsEarthquake/App.java
+++ src/main/java/eu/djakarta/tsEarthquake/App.java
@@ -4,10 +4,8 @@ package eu.djakarta.tsEarthquake;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
