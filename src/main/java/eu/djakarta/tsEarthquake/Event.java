4493137c341bc9f0a6b4219d14b8d22f30f48f3a
Cristescu Bogdan Ion <bogdan.cristescu@dcsi.eu>
2017-08-26 02:11:51 +0300

    Updated event to use proper time

--- src/main/java/eu/djakarta/tsEarthquake/Event.java
+++ src/main/java/eu/djakarta/tsEarthquake/Event.java
@@ -1,17 +1,31 @@
 package eu.djakarta.tsEarthquake;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import javax.xml.bind.DatatypeConverter;
+
 public class Event {
   public final double magnitude;
-  public final double time;
+  public final Date time;
   public final double latitude;
   public final double longitude;
   public final double depth;
 
-  public Event(double magnitude, double time, double latitude, double longitude, double depth) {
+  public Event(double magnitude, Date time, double latitude, double longitude, double depth) {
     this.magnitude = magnitude;
     this.time = time;
     this.latitude = latitude;
     this.longitude = longitude;
     this.depth = depth;
+  }
+
+  @Override
+  public String toString() {
+    Calendar calendar = GregorianCalendar.getInstance();
+    calendar.setTime(time);
+    return "Event [magnitude=" + magnitude + ", time=" + DatatypeConverter.printDateTime(calendar)
+        + ", latitude=" + latitude + ", longitude=" + longitude + ", depth=" + depth + "]";
   }
 }
