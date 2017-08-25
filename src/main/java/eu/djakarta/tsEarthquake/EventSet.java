d2bf02302df339bf2bcb681903554ebb32d70e81
Cristescu Bogdan Ion <bogdan.cristescu@dcsi.eu>
2017-08-26 02:12:28 +0300

    Updated set to use proper comparators and added methods for sorted lists

--- src/main/java/eu/djakarta/tsEarthquake/EventSet.java
+++ src/main/java/eu/djakarta/tsEarthquake/EventSet.java
@@ -1,16 +1,25 @@
 package eu.djakarta.tsEarthquake;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 public class EventSet {
   private List<Event> magnitudeAscending;
-  private List<Event> dateAscending;
+  private List<Event> timeAscending;
   
   public EventSet(List<Event> list) {
     this.magnitudeAscending = new LinkedList<>(list);
     this.magnitudeAscending.sort(new MagnitudeEventComparator());
-    this.dateAscending = new LinkedList<>(list);
-    this.dateAscending.sort(new MagnitudeEventComparator());
+    this.timeAscending = new LinkedList<>(list);
+    this.timeAscending.sort(new TimeEventComparator());
+  }
+  
+  public List<Event> magnitudeAscendingSortedList() {
+    return Collections.unmodifiableList(this.magnitudeAscending);
+  }
+  
+  public List<Event> timeAscendingSortedList() {
+    return Collections.unmodifiableList(this.timeAscending);
   }
 }
